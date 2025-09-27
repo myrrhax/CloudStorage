4b4cefc604ab5b40a3fde83017ab7c3cfcfda6d3
Денис Сизинцев <sizintsevdenis05@mail.ru>
2025-09-27 20:28:38 +0300

    role repository

--- src/main/java/com/myrr/CloudStorage/repository/RoleRepository.java
+++ src/main/java/com/myrr/CloudStorage/repository/RoleRepository.java
@@ -1,7 +1,9 @@
 package com.myrr.CloudStorage.repository;
 
 import com.myrr.CloudStorage.model.entity.Role;
+import com.myrr.CloudStorage.model.enums.RoleType;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RoleRepository extends CrudRepository<Role, Integer> {
+    Role findByRole(RoleType role);
 }
