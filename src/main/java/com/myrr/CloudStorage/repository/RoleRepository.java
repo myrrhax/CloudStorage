374cc9fd7564de995fc4a7770fd845d86e61a527
Денис Сизинцев <sizintsevdenis05@mail.ru>
2025-09-27 19:37:19 +0300

    create base repositories

---
+++ src/main/java/com/myrr/CloudStorage/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.myrr.CloudStorage.repository;
+
+import com.myrr.CloudStorage.model.entity.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<Role, Integer> {
+}
